dc256b87bcfc59a4a87ccc6598aa4abb437e40d9
Eric-Orfano <orfano.eric@gmail.com>
2024-02-16 08:57:58 -0300

    ain

--- Bucles/app.js
+++ Bucles/app.js
@@ -55,3 +55,113 @@
 
 // Ejercicio 4
 
+// let suma = 0;
+// let numero;
+
+// do {
+//     numero = parseInt(prompt("Ingrese un número (ingrese 0 para terminar):"));
+
+//     if (!isNaN(numero)) {
+//         suma += numero;
+
+//         console.log("Suma actual:", suma);
+//     } else {
+//         console.log("Por favor, ingrese un número válido.");
+//     }
+
+// } while (numero !== 0);
+
+// console.log("Resultado final de la suma:", suma);
+
+// Ejercicio 5 
+
+// let persona = {
+//     nombre: "Eric",
+//     edad: 26,
+//     ciudad: "Ciudad de Rio Negro",
+//     profesion: "programador",
+//     hobbies: ["video juegos", "softbol"]
+// };
+
+// for (let key in persona) {
+//     console.log(key);
+// }
+
+
+// Ejercicio 6
+
+// let persona = {
+//     nombre: "Eric",
+//     edad: 26,
+//     ciudad: "Ciudad de Rio Negro",
+//     profesion: "programador",
+//     hobbies: ["video juegos", "softbol"]
+// };
+
+// for (let key in persona) {
+//     console.log(`${persona[key]}`);
+// }
+
+// Nivel Medio
+// Ejercicio 1
+
+// let numeroSecreto = 4;
+// let suma = 0;
+// let intentos = 0;
+// let numeroIngresado = parseInt(prompt("Intenta adivinar el número secreto. Ingresa un número:"));
+
+// while (numeroIngresado !== numeroSecreto) {
+//     intentos++;
+
+//     if (numeroIngresado > numeroSecreto) {
+//         console.log("El número ingresado es mayor. Intenta de nuevo.");
+//     } else {
+//         console.log("El número ingresado es menor. Intenta de nuevo.");
+//     }
+
+//     numeroIngresado = parseInt(prompt("Ingresa otro número:"));
+// }
+
+// console.log("¡Felicitaciones! Adivinaste el número secreto en " + intentos + " intentos.");
+
+// Ejercicio 2
+
+// let numero = parseInt(prompt("Ingrese un número para encontrar sus divisores:"));
+
+// console.log("Divisores de " + numero + ":");
+
+// for (let i = 1; i <= numero; i++) {
+//     if (numero % i === 0) {
+//         let cociente = numero / i;
+//         console.log(i + " es divisor. Resultado es " + cociente + " y sobra 0.");
+//     }
+// }
+
+// Ejercicio 3
+
+// function sonarCampana(){
+//     console.log("Ding Dong")
+//     return "Ding Dong,"
+// }
+
+// let repeticiones = 5;
+// let resultado = "";
+
+// for (let i = 0; i < repeticiones; i++) {
+//     resultado += sonarCampana();
+// }
+
+// console.log(resultado);
+
+// Ejercicio 4 
+
+const fechaLimite = "1997-8-3"
+const fechas = ["1984-18-2", "1998-3-2", "1973-15-7", "2024-21-10", "2001-1-9"]
+
+const fechaLimiteObj = new Date(fechaLimite);
+for (const fecha of fechas) {
+    const fechaActualObj = new Date(fecha);
+    if (fechaActualObj >= fechaLimiteObj) {
+        console.log(fecha);
+    }
+}
